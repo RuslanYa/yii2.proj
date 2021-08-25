0210dc58827735dde17518020b1b9d450a22190a
rusprus <kazanpiter@gmail.com>
2021-08-26 00:30:42 +0300

    Зафиксировал проект 1.5 годичной давности  yii2

---
+++ config/db.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+   'class' => 'yii\db\Connection',
+   'dsn' => 'mysql:host=localhost;dbname=yii2_loc',
+   'username' => 'admin',
+   'password' => '6mhHXb8pFAaM',
+   'charset' => 'utf8',
+
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=cg48799_shop',
+    // 'username' => 'cg48799_shop',
+    // 'password' => '1tkT30gkUI8s',
+    //'password' => 'uAvX2diQ',
+    
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
